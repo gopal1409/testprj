545b05fa4f8dc0bc13217ac9e53f912a538af2b3
gopal das <root@ip-172-31-31-137.us-east-2.compute.internal>
2018-03-29 13:24:08 +0000

    initial commit

---
+++ calc.py
@@ -0,0 +1,19 @@
+
+def add(x, y):
+    """Add Function"""
+    pass
+
+
+def subtract(x, y):
+    """Subtract Function"""
+    pass
+
+
+def multiply(x, y):
+    """Multiply Function"""
+    pass
+
+
+def divide(x, y):
+    """Divide Function"""
+    pass
